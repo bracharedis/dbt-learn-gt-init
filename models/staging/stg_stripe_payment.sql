efa8a4c362d19ddace1a78664116d5554acafb7d
Bracha Shimonovitz <35485915+bracharedis@users.noreply.github.com>
2025-11-03 15:38:41 +0200

    my_second_commit

--- models/staging/stg_stripe_payment.sql
+++ models/staging/stg_stripe_payment.sql
@@ -1,8 +1,8 @@
-select id
+select id as payment_id
     ,orderid as order_id
-    ,paymentmethod
+    ,paymentmethod as payment_method
     ,status
-    ,amount
+    ,amount / 100 as amount  -- in cents convert to dollars
     ,created
     ,_batched_at
 from raw.stripe.payment
